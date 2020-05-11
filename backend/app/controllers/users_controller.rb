8f43b08422b0857cf5ed336b14e50c405d05dc64
Hatem <hatem.deux@gmail.com>
2020-05-11 17:04:57 -0400

    init first functioning route úsers'along with its model and controller

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  
+  # before_action :set_user, only: [:show, :update, :destroy]
+  # before_action :set_user
+
+  # GET /users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  # POST /users
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  
+  # GET /users/:id
+  def show
+    json_response(@user)
+  end
+
+  # STRETCH
+  # PUT /users/:id
+  # def update
+  #   @user.update(user_params)
+  #   head :no_content
+  # end
+
+  # STRETCH
+  # DELETE /users/:id
+  # def destroy
+  #   @user.destroy
+  #   head :no_content
+  # end
+
+  private
+
+  def user_params
+    # whitelist params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
