e0e02a444cf802ac0a430a97831a1823c138e379
Lee Castelani <notayogilee@gmail.com>
2020-05-12 16:28:13 -0400

    add component plantList

--- client/src/components/PlantListItem.js
+++ client/src/components/PlantListItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
+
 export default function PlantListItem(props) {
   return (
-    <div>
-      <section>
 
-        <h2>{props.name}</h2>
-        <img src={props.img}></img>
-        <p>{props.description}</p>
-        <p>{props.watering}</p>
-      </section>
+    <div>
+
+      <h2>{props.name}</h2>
+      <img src={props.img}></img>
+      <p>{props.description}</p>
+      <p>{props.watering}</p>
+      <p>{props.feeding}</p>
+      <p>{props.diseases}</p>
+      <p>{props.growing_from_seed}</p>
+      <p>{props.harvesting}</p>
+      <p>{props.optimal_soil}</p>
+      <p>{props.other_care}</p>
+      <p>{props.pests}</p>
+      <p>{props.planting_considerations}</p>
+      <p>{props.spacing}</p>
+      <p>{props.storage}</p>
+      <p>{props.transplanting}</p>
+      <p>{props.when_to_plant}</p>
+
     </div>
   )
 }
