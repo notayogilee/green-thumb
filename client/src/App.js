6a5156cded2876627b5d3f26694c5279b5b32683
Lee Castelani <notayogilee@gmail.com>
2020-05-14 11:51:51 -0400

    add feature seach bar to filter plants

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import PlantCard from './components/PlantCard';
 import Register from './components/Register';
 import Login from './components/Login';
 
 import './App.css';
 import PlantListItem from './components/PlantListItem';
+import PlantCardList from './components/PlantCardList';
 
 
 function App() {
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useGardenData from '../hooks/useGardenData';
 import PlantCard from './PlantCard';
+import SearchResults from './SearchResults';
+import PlantCardList from './PlantCardList';
 
-export default function Garden(props) {
+export default function Home(props) {
+
+  const { state } = useGardenData();
+
+  const [searchPlant, setSearchPlant] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = event => {
+    setSearchPlant(event.target.value);
+  };
+
+  useEffect(() => {
+
+    const results = state.plants.filter(plant =>
+      plant.name.toLowerCase().includes(searchPlant)
+    );
+    setSearchResults(results);
+  }, [searchPlant]);
+
+  // function userSearch(props) {
+  //   if()
+  // }
 
   return (
 
-    <PlantCard />
+    <div>
+      <input
+        type="text"
+        value={searchPlant}
+        placeholder="Which plant would you like?"
+        onChange={handleChange}>
+      </input>
+      {searchPlant.length === 0 &&
 
+        <PlantCard />
+      }
+      <ul>
+        {searchResults.length > 0 && searchResults.map(plant => (
+          <SearchResults
+            key={plant.id}
+            name={plant.name}
+            img={plant.image_url}
+          />
+        ))}
+      </ul>
+      {(searchResults.length === 0 && searchPlant.length > 0) &&
+        <h2>That plant was not found!</h2>
+      }
+
+
+
+    </div>
   )
 }
