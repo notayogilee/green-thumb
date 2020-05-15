01877c60fe5add26c35ffb4c2183d9e4af131c3d
Hatem <hatem.deux@gmail.com>
2020-05-14 21:58:11 -0400

    adds logout and checks if user is logged_in

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # ORDER MATTERS !!
+  # Be careful: Order Matters!
+
+  ## Routes : Registrations, Login, Logout & cookies
+
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "session#logged_in"
+
+  ## Routes : Gardens & Plants
   
   get '/gardens' => 'gardens#all_gardens'
   get '/plants', to: 'plants#all_plants'
