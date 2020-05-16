0c614b2c92d81d8513aaf572ae370daccc070884
Hatem <hatem.deux@gmail.com>
2020-05-16 13:41:12 -0400

    fixes typo sessions#logged_in

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
-  get :logged_in, to: "session#logged_in"
+  get :logged_in, to: "sessions#logged_in"
 
   ## Routes : Gardens & Plants
   
