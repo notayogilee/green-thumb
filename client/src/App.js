dd9203c05d8fde4b4ad54aa529a71399de7cddb8
Hatem <hatem.deux@gmail.com>
2020-05-16 12:51:17 -0400

    adds react routes Login_NEW and Register_NEW, comments out existing ones

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
+// import Register from './components/Auth/Register';
+import Register_NEW from './components/Auth/Register_NEW';
+// import Login from './components/Auth/Login';
+import Login_NEW from './components/Auth/Login_NEW';
 
 import './App.css';
 import PlantListItem from './components/PlantListItem';
@@ -11,14 +13,16 @@ import PlantListItem from './components/PlantListItem';
 
 function App() {
 
-
   return (
 
     <div>
       <Navbar />
       <Switch>
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
+
+        <Route path='/login' component={Login_NEW} />
+        {/* <Route path='/login' component={Login} /> */}
+        {/* <Route path='/register' component={Register} /> */}
+        <Route path='/register' component={Register_NEW} />
         <Route path='/' component={Home} />
       </Switch>
 
