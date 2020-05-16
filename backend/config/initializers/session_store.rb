d3b8f9e5f6db46882ba68e72f92572caafc2ac84
Hatem <hatem.deux@gmail.com>
2020-05-16 12:48:16 -0400

    minor comment edit

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_green_thumb_app"#, domain: "your-production-app-here.com"
+Rails.application.config.session_store :cookie_store, key: '_green_thumb_app'#, domain: 'your-production-app-here.com'
